8ccf998b030eb7f705a5c553fdc23a01d291bde2
Lokesh Sharma <lokeshls8680@gmail.com>
2023-10-21 21:51:49 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,83 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const skillData = [
+    {
+        skill: "HTML+CSS",
+        level: "advanced",
+        color: "#2662EA"
+    },
+    {
+        skill: "JavaScript",
+        level: "advanced",
+        color: "#EFD81D"
+    },
+    {
+        skill: "Web Design",
+        level: "advanced",
+        color: "#C3DCAF"
+    },
+    {
+        skill: "Git and GitHub",
+        level: "intermediate",
+        color: "#E84F33"
+    },
+    {
+        skill: "React",
+        level: "advanced",
+        color: "#60DAFB"
+    },
+    {
+        skill: "Svelte",
+        level: "beginner",
+        color: "#FF3B00"
+    }
+];
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+    return <div className="card">
+        <Avatar />
+
+        <div className="data">
+            <Intro />
+
+            <SkillList />
+        </div>
+    </div>
+}
+function Avatar() { return <img className="photo" src="info.jpeg" alt="profile" /> }
+
+function Intro() {
+    return <div>
+        <h1>Lokesh Sharma </h1>
+        <p>Full-stack webdeveloper and cybersecurity enthu.I like to play board games, to cook(and eat),or to just enjoy
+            the portuguese sun at beach. </p>
+    </div>
+}
+
+function SkillList() {
+    return (
+        <div className="skill-list">
+            {skillData.map((skills) =>
+                (<Skill skillobj={skills} key={skills.skill} />)
+            )}
+        </div>
+    );
+}
+
+function Skill({ skillobj }) {
+    return (
+        <div className="skill" style={{ backgroundColor: skillobj.color }}>
+            <span>{skillobj.skill}</span>
+            <span>{(skillobj.level === 'beginner' && "👶")}
+                {skillobj.level === "intermediate" && "👍"}
+                {skillobj.level === "advanced" && "💪"}
+            </span>
+        </div>
+    );
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
